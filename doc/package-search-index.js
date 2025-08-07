9f009ed594b892bdf48920ee2f5f4d5a5776d903
rubychen6079 <rubychen6079@gmail.com>
2025-08-07 09:59:27 +0800

    20250807new

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"controller.boss"},{"l":"controller.member"},{"l":"controller.sale"},{"l":"dao"},{"l":"dao.impl"},{"l":"model"},{"l":"service"},{"l":"service.impl"},{"l":"util"}];updateSearchResults();
